ec8a91eec181529d21bbf0c0e47c6356200c7625
Asım Atasert <asimatasert@outlook.com>
2020-05-24 16:15:09 +0300

    Create tr.js

---
+++ tr.js
@@ -0,0 +1,95 @@
+export default {
+  dofus_touch: "Dofus Touch",
+  menu: {
+    lindo: "Lindo",
+    help: "Yardım",
+    discord: "Discord",
+    github: "GitHub"
+  },
+  index: {
+    page: {
+      title: "Lindo No-Emu,Dofus Touch için",
+      description: "Dofus Touch'ı Windows / Mac OS / Linux işletim sistemlerinde emülatörlerden daha hızlı ve daha pürüzsüz oynayın"
+    },
+    description: "{dofus_touch}'u Oynamaya başla Windows / Mac OS / Linux",
+    cta: {
+      download: "İndir",
+      info: "Lindo nedir?"
+    },
+    feature: {
+      fast: {
+        title: "Emülatörkerden daha hızlı",
+        description: "Lindo \"No-Emu\". Dofus Touch'ı çalıştırmak, Android emülatörü yada eliacube'un gücü değildir. Bilgisayarınızda Dofus Touch'ı çalıştırmak için en son ve en iyi PC yapılandırmasına ihtiyacınız olmayacak."
+      },
+      shortcuts: {
+        title: "Klavye kısayollarıyla",
+        description: "Farenizin küçük eliniz için çok ağır olduğunu biliyoruz ve her büyü için sürüklemenize gerek yok. Sizi düşündük, büyülerinizi klavyeden seçebilir, menülere erişebilir, sıranızı atlayabilir ve hatta can sıkıcı arkadaşınızı reddedebilirsiniz. Her kısayol elbette özelleştirilebilir."
+      },
+      open_source: {
+        title: "Herşey Açık Kaynak",
+        description: "Lindo topluluk tarafından geliştirilen açık bir yazılımdır, herkes {github} 'a giderek nasıl kurulduğunu görebilir ve gelişimine katkıda bulunabilir. Hesap güvenliği önemlidir. Gerçekten mi?. Hala ikna olmadıysanız, gelip {discord} 'a merhaba deyin.",
+        github: "GitHub",
+        github_url: "https://github.com/prixe/lindo",
+        discord: "Discord",
+        discord_url: "https://discord.gg/wcCgtsv"
+      }
+    },
+    team: {
+      title: "Katkıda bulunanlar"
+    },
+    footer: {
+      disclaimer: "Bu yazılım Ankama tarafından kullanımı ve geliştirilmesine katkıda bulunmak üzere, {tos}, Madde 5.2'de belirtildiği şekilde yasaklanmıştır. Lütfen bunu adil bir şekilde kullanmayı unutmayın, oyuna toksik davranmaktan kaçının ve hesabınızın yasaklanma riskini almayın.",
+      tos_url: "https://www.dofus-touch.com/en/tou",
+      tos_text: "Kullanım Şartları",
+      copyright: "© 2020 Lindo ekibi",
+      credits: "Web sitesi {clover} tarafından oluşturulmuştur.",
+      clover_url: "https://github.com/Clover-Lindo",
+      clover_text: "Clover",
+      credits_ankama: "Arka plan, karakterler ve tüm Dofus Touch oyunu {ankama} 'nın fikri mülkiyetidir.",
+      ankama_url: "https://www.ankama.com/",
+      ankama_text: "Ankama"
+    }
+  },
+  help: {
+    page: {
+      title: "Yardım - Lindo No-Emu, Dofus Touch için",
+      description: "Lindo hakkında yardım ve sık sorulan sorular. Dofus Touch'ı Windows / Mac OS / Linux işletim sistemlerinde emülatörlerden daha hızlı ve daha pürüzsüz oynayın"
+    },
+    title: "Bir sorunuz mu var?",
+    search_placeholder: "Arama",
+    discord_title: "Cevabınızı bulamadınız mı? Bize Discord'da sorun!",
+    sections: {
+      webgl: {
+        title: "\"Cihazınız WebGL'yi desteklemiyor\"",
+        text: "Bu mesaj, ekran kartınızın Dofus Touch tarafından kullanılan görüntüleme teknolojisi olan WebGL ile uyumlu olmadığı anlamına gelir. Ekran kartı sürücülerini güncelleyerek bu sorunu gidermek mümkündür. Hala çalışmıyorsa, daha yeni bir ekran kartı seçmeniz gerekir."
+      },
+      smartscreen: {
+        title: "Windows SmartScreen tanınmayan bir uygulamanın başlatılmasını engelledi.",
+        text: "Uygulama bazen tanınmış bir düzenleyici tarafından resmi olarak onaylanmadığından Windows bazen bu mesajı görüntüleyebilir. \"Ek bilgi\" ye tıklayarak bu mesajı göz ardı edebilirsiniz.\"More info\"."
+      },
+      white_screen: {
+        title: "Bir süre sonra ekranım beyaz görünüyor",
+        text: "Lindo'da birkaç saat oynadıktan sonra olabilir. Birçok hesapla oynuyorsanız, bunları farklı pencerelere ayırmaya çalışın."
+      },
+      servers: {
+        title: "Tabletlerinden veya akıllı telefonlarından oynayan arkadaşlarımla oynayabilir miyim?",
+        text: "Evet! Aslında, Lindo aynı Dofus Touch sürümünü kullanır ve böylece kendisini aynı sunuculara bağlar. Mobil cihazlarda bulunan diğer tüm oyuncularla oynayacaksınız."
+      },
+      allowed: {
+        title: "Lindo'yu Ankama izin veriyor mu?",
+        text: "Ne yazık ki hayır. Belirli bir şekilde tolere edilir, ancak birden fazla hesapla oynarsanız veya Lindo'yu otomatik takip özelliği (Lindo seçeneklerinde manuel olarak etkinleştirilebilir) gibi ek özellikler kullanırsanız, Ankama daha az toleranslı olur ve daha fazla risk alırsınız."
+      },
+      update: {
+        title: "Güncelleme asla bitmiyor",
+        text: `Dofus Touch güncellemesi sırasında sorunla karşılaşırsanız, oyun dosyalarını içeren klasörü silin. Aşağıdaki klasörlerden biridir:
+ 
+Windows : <code>%appdata%\\lindo</code> (bunu Windows arama çubuğuna yazın)
+Mac : <code>~/Library/Application Support/lindo/</code>
+Linux : <code>~/.config/lindo</code>`
+      }
+    }
+  },
+  downloads: {
+    changelog: "Changelog"
+  }
+}
